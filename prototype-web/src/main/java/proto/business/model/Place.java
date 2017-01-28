21b3ab10a8e6afc067fe50683f031abb229a9689
ecaillat <emmanuel.caillat@gmail.com>
2017-01-28 15:35:59 +0100

    Frnont-end Angular JS

--- prototype-web/src/main/java/proto/business/model/Place.java
+++ prototype-web/src/main/java/proto/business/model/Place.java
@@ -23,6 +23,18 @@ public class Place {
 	 * The coordinates of the place
 	 */
 	private String coordinates;
+	/**
+	 * The Google Place ID
+	 */
+	private String googlePlaceId;
+	/**
+	 * The description
+	 */
+	private String description;
+	/**
+	 * The address
+	 */
+	private String address;
 
 	/**
 	 * @return the name
@@ -52,6 +64,51 @@ public class Place {
 	 */
 	public void setCoordinates(String coordinates) {
 		this.coordinates = coordinates;
+	}
+
+	/**
+	 * @return the googlePlaceId
+	 */
+	public String getGooglePlaceId() {
+		return googlePlaceId;
+	}
+
+	/**
+	 * @param googlePlaceId
+	 *            the googlePlaceId to set
+	 */
+	public void setGooglePlaceId(String googlePlaceId) {
+		this.googlePlaceId = googlePlaceId;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 }
